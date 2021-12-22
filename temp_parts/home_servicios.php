0b6c08efff3aa419d1d8460e7ac7cef927c6895e
Javier Ramos <javerne@gmail.com>
2021-12-22 17:10:24 -0400

    Primer approach a slider de servicios

--- temp_parts/home_servicios.php
+++ temp_parts/home_servicios.php
@@ -8,43 +8,161 @@ $posts_slide = new WP_Query(array(
 $i=0;
 $e=0;
 ?>
-<div class="container-slider bg-danger">
-        <div class="col-lg-12">
-            <div id="miCarousel" class="carousel slide" data-ride="carousel">
-                <!-- Indicadores -->
-                <ol class="carousel-indicators">
-                    <?php while ($posts_slide->have_posts()) : $posts_slide->the_post();?>
+<div class="container-slider">
+    <div id="miCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Contenedor de los Slider -->
+        <div class="carousel-inner h-100"
+            style="background: right bottom no-repeat url(<?= mesh4all_IMG.'services/wave-color-up-350h.png)'?>">
 
-                    <li data-target="#miCarousel" data-slide-to="<?php echo $e; ?>" <?php if($e == 0) : ?> class='active'<?php endif;?>>
-                    </li>
-                        <?php $e++; endwhile;?>
-                </ol>
-                
-                <!-- Contenedor de los Slider -->
-                <div class="carousel-inner" role="listbox">
+        <?php 
+        if ($posts_slide->have_posts()){
+            while($posts_slide->have_posts()) : $posts_slide->the_post(); ?>
+                <div class="item <?php if($i == 0) echo 'active';?>">
+                    <img class="" src='<?php the_post_thumbnail();?>'>
 
-                <?php while($posts_slide->have_posts()) : $posts_slide->the_post(); ?>
-                    <div class="item <?php if($i == 0) echo 'active';?>">
-                        <img class="" src='<?php the_post_thumbnail();?>'>
-
-                        <div class="carousel-caption visible-lg">
-                            <h3 class="bg-success"><?php the_title(); ?></h3>
-                            <!--p><?php the_excerpt(); ?></p-->
-                        </div>
-
+                    <div class="carousel-caption visible-lg">
+                        <h3 class="bg-success"><?php the_title(); ?></h3>
+                        <!--p><?php the_excerpt(); ?></p-->
                     </div>
-                    <?php $i++; endwhile; wp_reset_postdata();?>
-                </div>
 
-                <!-- Controles -->
-                <a class="carousel-control-prev" href="#miCarousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Anterior</span>
-                </a>
-                <a class="carousel-control-next" href="#miCarousel" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Siguiente</span>
-                </a>
+                </div>
+            <?php $i++; 
+            endwhile; 
+            wp_reset_postdata();
+        }else{
+            ?>
+            <div class="carousel-item active h-100">
+                <div class="col-7  h-100">
+                    <div class="col-12 row p-0 m-0  w-100 h-25">
+                        <div class="col-3">
+                            &nbsp;
+                        </div>
+                        <div class="col-9 d-flex align-items-center ">
+                            <h1>SERVICIOS</h1>
+                        </div>
+                    </div>
+                    <div class="col-12 row  p-0 m-0  
+                                d-flex align-items-center justify-content-center">
+                        <img class="d-block w-50" 
+                            src="<?= mesh4all_IMG.'services/control-red-cutted.png'?>" alt="First slide">
+                    </div>
+                </div>
+                <div class="col-5">
+                    &nbsp;
+                </div>
             </div>
+            <div class="carousel-item h-100">
+                <div class="col-7  h-100">
+                    <div class="col-12 row p-0 m-0  w-100 h-25">
+                        <div class="col-3">
+                            &nbsp;
+                        </div>
+                        <div class="col-9 d-flex align-items-center ">
+                            <h1>SERVICIOS</h1>
+                        </div>
+                    </div>
+                    <div class="col-12 row  p-0 m-0  
+                                d-flex align-items-center justify-content-center">
+                        <img class="d-block w-50" 
+                            src="<?= mesh4all_IMG.'services/control-orange-cutted.png'?>" alt="First slide">
+                    </div>
+                </div>
+                <div class="col-5">
+                    &nbsp;
+                </div>
+            </div>
+            <div class="carousel-item h-100">
+                <div class="col-7  h-100">
+                    <div class="col-12 row p-0 m-0  w-100 h-25">
+                        <div class="col-3">
+                            &nbsp;
+                        </div>
+                        <div class="col-9 d-flex align-items-center ">
+                            <h1>SERVICIOS</h1>
+                        </div>
+                    </div>
+                    <div class="col-12 row  p-0 m-0  
+                                d-flex align-items-center justify-content-center">
+                        <img class="d-block w-50" 
+                            src="<?= mesh4all_IMG.'services/control-yellow-cutted.png'?>" alt="First slide">
+                    </div>
+                </div>
+                <div class="col-5">
+                    &nbsp;
+                </div>
+            </div>
+            <div class="carousel-item h-100">
+                <div class="col-7  h-100">
+                    <div class="col-12 row p-0 m-0  w-100 h-25">
+                        <div class="col-3">
+                            &nbsp;
+                        </div>
+                        <div class="col-9 d-flex align-items-center ">
+                            <h1>SERVICIOS</h1>
+                        </div>
+                    </div>
+                    <div class="col-12 row  p-0 m-0  
+                                d-flex align-items-center justify-content-center">
+                        <img class="d-block w-50" 
+                            src="<?= mesh4all_IMG.'services/control-green-cutted.png'?>" alt="First slide">
+                    </div>
+                </div>
+                <div class="col-5">
+                    &nbsp;
+                </div>
+            </div>
+            <div class="carousel-item h-100">
+                <div class="col-7  h-100">
+                    <div class="col-12 row p-0 m-0  w-100 h-25">
+                        <div class="col-3">
+                            &nbsp;
+                        </div>
+                        <div class="col-9 d-flex align-items-center ">
+                            <h1>SERVICIOS</h1>
+                        </div>
+                    </div>
+                    <div class="col-12 row  p-0 m-0  
+                                d-flex align-items-center justify-content-center">
+                        <img class="d-block w-50" 
+                            src="<?= mesh4all_IMG.'services/control-blue-cutted.png'?>" alt="First slide">
+                    </div>
+                </div>
+                <div class="col-5">
+                    &nbsp;
+                </div>
+            </div>
+            <div class="carousel-item h-100">
+                <div class="col-7  h-100">
+                    <div class="col-12 row p-0 m-0  w-100 h-25">
+                        <div class="col-3">
+                            &nbsp;
+                        </div>
+                        <div class="col-9 d-flex align-items-center ">
+                            <h1>SERVICIOS</h1>
+                        </div>
+                    </div>
+                    <div class="col-12 row  p-0 m-0  
+                                d-flex align-items-center justify-content-center">
+                        <img class="d-block w-50" 
+                            src="<?= mesh4all_IMG.'services/control-purple-cutted.png'?>" alt="First slide">
+                    </div>
+                </div>
+                <div class="col-5">
+                    &nbsp;
+                </div>
+            </div>
+            
+            <?php
+        }
+        ?>
         </div>
+
+        <!-- Controles -->
+        <a class="carousel-control-prev p-0 m-0" href="#miCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </a>
+        <a class="carousel-control-next p-0 m-0" href="#miCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </a>
+    </div>
 </div>
